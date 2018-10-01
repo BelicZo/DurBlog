77a50a284a2432099a93ec08bb1d5fbeaab6b90c
BelicZo <zxb2031053@outlook.com>
2018-10-01 23:45:44 +0800

    增加common APP

--- DurBlog/urls.py
+++ DurBlog/urls.py
@@ -27,11 +27,13 @@ urlpatterns = [
 ]
 
 from articles import views
-
+from articles.urls import articles_patterns
 urlpatterns += [
-    url('^blog/$', views.ArticlesListView.as_view(), name="blog_list"),
-    url('^blog/(?P<pk>[0-9]{1,3})/$', views.ArticlesDetailView.as_view(), name='blog_detail'),
-    url('^new_blog/$', views.ArticlesCreateView.as_view(), name="blog_create"),
+    # url('^blog/$', views.ArticlesListView.as_view(), name="blog_list"),
+    # url('^blog/(?P<pk>[0-9]{1,3})/$', views.ArticlesDetailView.as_view(), name='blog_detail'),
+    # url('^', include("articles.urls", namespace='blog')),
+    url('^', include(articles_patterns)),
+    # url('^new_blog/$', views.ArticlesCreateView.as_view(), name="blog_create"),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^markdownx/', include('markdownx.urls')),
     url(r'^martor/', include('martor.urls')),
